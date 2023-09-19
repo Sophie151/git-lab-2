5e513b47385af136cd5862161fd7685424d1f715
Sophie151 <sw030522@ohio.edu>
2023-09-19 17:21:32 -0400

    a

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Sophie Wilson
+ *        Date: 9/11/23
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
